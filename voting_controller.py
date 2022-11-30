1b18a29fbd5e9d19b65243f905b09770090a1b73
Mjames951 <aspc.james2@gmail.com>
2022-11-30 12:53:05 -0600

    prototype 2 option voting

--- voting_controller.py
+++ voting_controller.py
@@ -7,3 +7,23 @@ class Controller(QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setupUi(self)
+        self.pushButton.clicked.connect(lambda: self.setupVote())
+        self.description.setText('')
+        self.votebutton_john.clicked.connect(lambda: self.voted('John'))
+        self.votebutton_jane.clicked.connect(lambda: self.voted('Jane'))
+        self.votebutton_jane.setHidden(True)
+        self.votebutton_john.setHidden(True)
+        self.heading.setHidden(True)
+
+    def setupVote(self):
+        self.votebutton_john.setEnabled(True)
+        self.votebutton_jane.setEnabled(True)
+        self.heading.setEnabled(True)
+        self.pushButton.setHidden(True)
+        self.votebutton_jane.setHidden(False)
+        self.votebutton_john.setHidden(False)
+        self.heading.setHidden(False)
+
+    def voted(self, name):
+        self.description.setText(f'YOU VOTED FOR {name}!')
+
